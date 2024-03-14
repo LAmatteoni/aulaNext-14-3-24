d77b7b872b8e72c34b908e216737a77d3e510866
Lucca Matteoni <lam.245@hotmail.com>
2024-03-14 12:10:38 -0300

    Criação do layout de uma página

---
+++ layout/src/app/carrossel/carrossel.tsx
@@ -0,0 +1,10 @@
+const Carrossel = () =>{
+    return(
+        <>
+        <div className="content">
+            <div className="carrossel"></div>
+        </div>
+        </>
+    )
+}
+export default Carrossel;
---
+++ layout/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Card from "./card/card";
+import Carrossel from "./carrossel/carrossel";
+
+export default function Home() {
+  return (
+    <>
+      <Carrossel/>
+      <Card/>
+      <h1>Criando Layout</h1>
+    </>
+  );
+}
